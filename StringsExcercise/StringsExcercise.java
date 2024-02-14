47e05698ce960667ff0f1fee9e95008f33ddb5b8
ABDULLAH <abdullah555523@hotmail.com>
2024-02-14 09:48:07 +0400

    HiBye

--- StringsExcercise/StringsExcercise.java
+++ StringsExcercise/StringsExcercise.java
@@ -4,6 +4,15 @@ class StringExcercise{
 
         String Name = "Bob";
         System.out.println("Hello " + Name + "!");
+
+        System.out.println(" ////////////////////////////////////////////////////////////// ");
+
+        String a = "Hi";
+        String b = "Bye";
+        String ab = a + b + b + a;
+        System.out.println(ab);
+
+
     }
 
 }
