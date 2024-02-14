5fe90cba6f6429949e90ba1aef9cb18342f51e07
ABDULLAH <abdullah555523@hotmail.com>
2024-02-14 13:38:43 +0400

    Added the 3 cp of the last 2dig

--- StringsExcercise/StringExcercise5.java
+++ StringsExcercise/StringExcercise5.java
@@ -2,11 +2,9 @@ class StringExcercise5{
 
     public static String extraEnd(String word){
       return word.substring(word.length()-2) + word.substring(word.length()-2) + word.substring(word.length()-2);
-
-
     }
     public static void main(String[] args) {
-        String c = extraEnd("Hi");
+        String c = extraEnd("Abdullah");
         System.out.println(c);
     }
 }
