ac06ba14a26b27d9b39ef9b7f1b77592903f83e5
ABDULLAH <abdullah555523@hotmail.com>
2024-02-14 10:30:49 +0400

    added a file for the Hi Bye string task

---
+++ StringsExcercise/StringsExcercise2.java
@@ -0,0 +1,13 @@
+class StringExcercise{
+
+    public static void main(String[] args) {
+
+        String a = "Hi";
+        String b = "Bye";
+        String ab = a + b + b + a;
+        System.out.println(ab);
+
+
+    }
+
+}
