a102e204a04a9cfbb983043d54789ba0c8b7d30e
ABDULLAH <abdullah555523@hotmail.com>
2024-02-14 11:10:57 +0400

    removed son unnececary lines

--- StringsExcercise/StringsExcercise.java
+++ StringsExcercise/StringsExcercise.java
@@ -1,16 +1,9 @@
-class StringExcercise{
+class StringExcercise2{
 
     public static void main(String[] args) {
 
         String Name = "Bob";
         System.out.println("Hello " + Name + "!");
-
-        System.out.println(" ////////////////////////////////////////////////////////////// ");
-
-        String a = "Hi";
-        String b = "Bye";
-        String ab = a + b + b + a;
-        System.out.println(ab);
 
 
     }
