8dae71f7c41f1e13fe00ab2da740816a2a385b73
ABDULLAH <abdullah555523@hotmail.com>
2024-02-19 09:58:20 +0400

    added the reverse Array task

---
+++ ArrayExcercise/ArrayEx6.java
@@ -0,0 +1,24 @@
+/*
+* Given an array of ints length 3,
+* return a new array with the elements in reverse order,
+* so {1, 2, 3} becomes {3, 2, 1}.
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+* */
+import java.util.Arrays;
+class ArrayEx6{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        int[] reversedArr = new int[arr.length];
+
+        int j = 0;
+        for(int i = arr.length-1; i > -1; i--){
+            reversedArr[j] = arr[i];
+            j++;
+        }
+        System.out.print(Arrays.toString(reversedArr));
+    }
+
+}
